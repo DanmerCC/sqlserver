e28e2a93493754d64722c69ae9bdde6574c3c7f4
Danmer Ccoscco <danmerccoscco@gmail.com>
2022-04-14 13:00:51 -0500

    ruta de test

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $serverName = "PC2020-103\SQLEXPRESS"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"Northwind", "UID"=>"danmer", "PWD"=>"123456");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     //die( print_r( sqlsrv_errors(), true));
+}
+
+$sql = "SELECT * FROM Categories";
+$res = sqlsrv_query($conn,$sql);
+dd(sqlsrv_fetch_array($res));
+
+$dsn = "sqlsrv:Server=PC2020-103\SQLEXPRESS,1433;Database=Northwind";
+
+dd(new PDO($dsn, "danmer", "123456", [0,2,0,false]));
+dd(\DB::connection()->getPdo());
+    return view('welcome');
+});
+
+
+Route::get('/', function () {
     return view('welcome');
 });
